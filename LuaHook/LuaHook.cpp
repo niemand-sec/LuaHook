0bb4c9cacb808af4e38c28ee26140cf75278ab9d
Joel Noguera <noguera.sistemas@gmail.com>
2019-05-29 17:42:26 -0300

    Hook adapted to ACTF working

--- LuaHook/LuaHook.cpp
+++ LuaHook/LuaHook.cpp
@@ -5,16 +5,29 @@
 #include <windows.h>
 #include <iostream>
 #include <iomanip>
-#include "detours.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "MinHook.h"
+#include <tlhelp32.h>
+#include <Psapi.h>
+
+#if defined _M_X64
+#pragma comment(lib, "libMinHook-x64-v141-mdd.lib")
+#elif defined _M_IX86
+#pragma comment(lib, "libMinHook-x86-v141-mdd.lib")
+#endif
+
+
 
 
 typedef void* lua_State;
 
 // LUA STATE HOOK DEFINITIONS
+uint64_t baseAddress = 0;
+uintptr_t targetFunction = 0;
 lua_State * lua_State_ptr = 0;
 lua_State * new_lua_State_ptr = 0;
+
 typedef int(__cdecl *gettop)(int);
 typedef lua_State *(__cdecl *newThread)(lua_State *);
 typedef void(__cdecl *pushinteger)(lua_State *, int);
@@ -22,6 +35,9 @@ typedef int(__cdecl *tointeger)(lua_State *, int);
 typedef int(__cdecl *_luaStatus)(lua_State *);
 typedef int(__cdecl *_luaL_loadstring)(lua_State *, const char *);
 //typedef int(__cdecl *_lua_pcall)(lua_State *, int, int, int);
+
+
+gettop origTargetFunction = NULL;
 
 
 typedef int(*_luaL_loadfilex)(lua_State *L, const char *filename, const char *mode);
@@ -45,155 +61,154 @@ typedef LONG(NTAPI *NtResumeProcess)(IN HANDLE ProcessHandle);
 void ConsoleSetup()
 {
 	AllocConsole();
-	SetConsoleTitle("Vermintide LUA hook by Niemand");
+	SetConsoleTitle("[+] LUA hook by Niemand");
 	freopen("CONOUT$", "w", stdout);
 	freopen("CONOUT$", "w", stderr);
 	freopen("CONIN$", "r", stdin);
 	std::cout << "Injecting..." << std::endl;
 }
 
-struct gameOffsets {
-	uintptr_t getTop = 0xe160;
-	uintptr_t newThread = 0xC9A0;
-	uintptr_t pushinteger = 0xCF70;
-	uintptr_t tointeger = 0xD6D0;
-	uintptr_t luaStatus = 0xE370;
-	uintptr_t loadstring = 0x40880;
-	uintptr_t pCall = 0xBB20;
-	uintptr_t loadfilex = 0x40980;
-}offsets;
 
-struct values {
-	uintptr_t lua51Module;
-	uintptr_t getTop;
-	uintptr_t newThread_f;
-	gettop lua_gettop_p;
-	newThread lua_newthread_p;
-	pushinteger lua_pushinteger_p;
-	uintptr_t lua_pushinteger_f;
-	tointeger lua_tointeger_p;
-	uintptr_t lua_tointeger_f;
-	uintptr_t lua_status_f;
-	_luaStatus lua_status_p;
-	uintptr_t luaL_loadstring_f;
-	_luaL_loadstring luaL_loadstring_p;
-	uintptr_t lua_pcall_f;
-	_lua_pcall lua_pcall_p;
-	uintptr_t luaL_loadfilex_f;
-	_luaL_loadfilex luaL_loadfilex_p;
-}val;
-
+gettop newFunction = NULL;
 
 DWORD _gettop(int state)
 {
-	//std::cout << "Hook called :O" << std::endl;
-	if (lua_State_ptr == 0) {
+	std::cout << "[!] Hook called :O" << std::endl;
+	if (lua_State_ptr == 0) 
+	{
 		lua_State_ptr = (lua_State *)state;
 		std::cout << "[+] State Obtained: \t" << std::hex << lua_State_ptr << std::endl;
+		if (MH_DisableHook((LPVOID)targetFunction) != MH_OK)
+		{
+			std::cout << "[+] Hook disabled" << std::endl;
+		}
 	}
-	return (*(DWORD *)(state + 24) - *(DWORD *)(state + 16)) >> 3;
+	return (*(DWORD *)(state + 0x18) - *(DWORD *)(state + 0x10)) >> 3;
 }
 
 
-void detourLuaState()
+
+//Get all module related info, this will include the base DLL.
+//and the size of the module
+MODULEINFO GetModuleInfo(char *szModule)
 {
-	//_gettop(lua_State); i don't think you need this but if you do you can just add it!
-	std::cout << "[+] Calling LuaState Detour" << std::endl;
-	DetourTransactionBegin();
-	DetourUpdateThread(GetCurrentThread());
-	DetourAttach(&(LPVOID&)val.lua_gettop_p, (PBYTE)_gettop); // Detours the original lua_gettop_p with our _gettop
-	DetourTransactionCommit();
+	MODULEINFO modinfo = { 0 };
+	HMODULE hModule = GetModuleHandle(szModule);
+	if (hModule == 0)
+		return modinfo;
+	GetModuleInformation(GetCurrentProcess(), hModule, &modinfo, sizeof(MODULEINFO));
+	return modinfo;
 }
 
-
-void retrieveValues()
+//Pattern Scan https://guidedhacking.com/threads/c-signature-scan-pattern-scanning-tutorial-difficulty-3-10.3981
+uintptr_t FindPattern(char *module, char *pattern, char *mask)
 {
-	val.lua51Module = (uintptr_t)GetModuleHandle("lua51.dll");
-	val.getTop = (uintptr_t)(val.lua51Module + offsets.getTop);
-	val.lua_gettop_p = (gettop)val.getTop; // this is the actual lua_gettop function in memory that we place the detour at
+	//Get all module related information
+	MODULEINFO mInfo = GetModuleInfo(module);
+	
+	//Assign our base and module size
+	uintptr_t base = (uintptr_t)mInfo.lpBaseOfDll;
 
-	val.newThread_f = (uintptr_t)(val.lua51Module + offsets.newThread);
-	val.lua_newthread_p = (newThread)val.newThread_f;
+	uintptr_t size = (uintptr_t)mInfo.SizeOfImage;
 
-	val.lua_pushinteger_f = (uintptr_t)(val.lua51Module + offsets.pushinteger);
-	val.lua_pushinteger_p = (pushinteger)val.lua_pushinteger_f;
+	//Get length for our mask, this will allow us to loop through our array
+	DWORD patternLength = (DWORD)strlen(mask);
 
-	val.lua_tointeger_f = (uintptr_t)(val.lua51Module + offsets.tointeger);
-	val.lua_tointeger_p = (tointeger)val.lua_tointeger_f;
+	for (DWORD i = 0; i < size - patternLength; i++)
+	{
+		bool found = true;
+		for (DWORD j = 0; j < patternLength; j++)
+		{
+			//if we have a ? in our mask then we have true by default,
+			//or if the bytes match then we keep searching until finding it or not
+			found &= mask[j] == '?' || pattern[j] == *(char*)(base + i + j);
+		}
 
-	val.lua_status_f = (uintptr_t)(val.lua51Module + offsets.luaStatus);
-	val.lua_status_p = (_luaStatus)val.lua_status_f;
-
-	//40880
-	val.luaL_loadstring_f = (uintptr_t)(val.lua51Module + offsets.loadstring);
-	val.luaL_loadstring_p = (_luaL_loadstring)val.luaL_loadstring_f;
-
-	val.luaL_loadfilex_f = (uintptr_t)(val.lua51Module + offsets.loadfilex);
-	val.luaL_loadfilex_p = (_luaL_loadfilex)val.luaL_loadfilex_f;
-
-	//BB20
-	val.lua_pcall_f = (uintptr_t)(val.lua51Module + offsets.pCall);
-	val.lua_pcall_p = (_lua_pcall)val.lua_pcall_f;
-
-
+		//found = true, our entire pattern was found
+		if (found)
+		{
+			std::cout << "[+] Pattern found: " << base + i << std::endl;
+			return base + i;
+		}
+	}
+	std::cout << "[-] Pattern not found" << std::endl;
+	return NULL;
 }
+
+
 
 void printValues()
 {
-	std::cout << "getTop Addr: \t" << std::hex << val.getTop << std::endl;
-	std::cout << "fn lua_get_top_p: \t" << std::hex << val.lua_gettop_p << std::endl;
+	std::cout << "targetFunction Addr: \t" << std::hex << targetFunction << std::endl;
+	std::cout << "newFunction Addr: \t" << std::hex << &_gettop << std::endl;
+	/*
 	std::cout << "fn lua_newthread_p: \t" << std::hex << val.lua_newthread_p << std::endl;
 	std::cout << "fn lua_pushinteger_p: \t" << std::hex << val.lua_pushinteger_p << std::endl;
 	std::cout << "fn lua_tointeger_p: \t" << std::hex << val.lua_tointeger_p << std::endl;
 	std::cout << "fn lua_status_p: \t" << std::hex << val.lua_status_p << std::endl;
 	std::cout << "fn luaL_loadstring_p: \t" << std::hex << val.luaL_loadstring_p << std::endl;
 	std::cout << "fn lua_pcall_p: \t" << std::hex << val.lua_pcall_p << std::endl;
-
+	*/
 }
 
 
-int refKey = 0;
-lua_State * CreateThread()
-{
-	std::cout << "[+] Calling newThread" << std::endl;
-	lua_State * thread = (val.lua_newthread_p)(lua_State_ptr);
-	std::cout << "[+] New LuaState: " << thread << std::endl;
 
-	return thread;
-};
+int detourLuaState()
+{
+	//_gettop(lua_State); i don't think you need this but if you do you can just add it!
+	std::cout << "[+] Hooking targetFunction: " << (LPVOID)targetFunction << std::endl;
+
+	
+	// Create a hook for MessageBoxW, in disabled state.
+	if (MH_CreateHook((LPVOID)targetFunction, &_gettop, reinterpret_cast<LPVOID*>(&origTargetFunction)) != MH_OK)
+	{
+		std::cout << "[-] Hooking failed" << std::endl;
+		return 1;
+	}
+	std::cout << "[+] Hooked" << std::endl;
+	if (MH_EnableHook((LPVOID)targetFunction) != MH_OK)
+	{
+		return 1;
+	}
+	std::cout << "[+] Hooked enabled" << std::endl;
+	std::cout << "[+] origTargetFunction " << std::hex << origTargetFunction << std::endl;
+	return 0;
+	/*
+	DetourTransactionBegin();
+	DetourUpdateThread(GetCurrentThread());
+	DetourAttach(&(LPVOID&)val.lua_gettop_p, (PBYTE)_gettop); // Detours the original lua_gettop_p with our _gettop
+	DetourTransactionCommit();
+	*/
+}
+
+
 
 int WINAPI main()
 {
 	ConsoleSetup();
 
-	retrieveValues();
+	//newFunction = (gettop)_gettop;
+
+	baseAddress = (uintptr_t)GetModuleHandle(NULL);
+	char* test = new char[14];
+	char* test2 = new char[14];
+	strcpy(test, "\x48\x8B\x41\x18\x48\x2B\x41\x10\x48\xc1\xf8\x03\xc3");
+	strcpy(test2, "xxxxxxxxxxxxx");
+	targetFunction = FindPattern(NULL, test, test2);
+
 	printValues();
+
+	// Initialize MinHook.
+	if (MH_Initialize() != MH_OK)
+	{
+		return 1;
+	}
 
 	detourLuaState();
 
-	//Forcing Sleep so lua state is created.
-	Sleep(6000);
-	//Creating New Lua Thread based on hooked State
-	new_lua_State_ptr = CreateThread();
-	//std::cout << "[+] New LuaStatus: " << lua_status(new_lua_State_ptr) << std::endl;
-	//std::cout << "[+] newgettop: " << lua_gettop(new_lua_State_ptr) << std::endl;
-	std::cout << "[+] LuaStatus: " << val.lua_status_p(lua_State_ptr) << std::endl;
-	std::cout << "[+] New LuaStatus: " << val.lua_status_p(new_lua_State_ptr) << std::endl;
-	std::cout << "++" << std::endl;
+	Sleep(10000);
 
-
-	while (true)
-	{
-		if (GetAsyncKeyState(VK_F9) & 1)
-		{
-			val.luaL_loadfilex_p(lua_State_ptr, "main.lua", NULL) || val.lua_pcall_p(lua_State_ptr, 0, -1, 0);
-			std::cout << "[!] Main.lua Executed" << std::endl;
-			//std::cout << "[+] LuaStatus: " << val.lua_status_p(lua_State_ptr) << std::endl;
-			//std::cout << "[+] New LuaStatus: " << val.lua_status_p(new_lua_State_ptr) << std::endl;
-		}
-		Sleep(2000);
-	}
-
+	return 1;
 }
 
 
